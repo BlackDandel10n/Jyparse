1d8ce3c66c16c66c1c9c95a66a0d495a51d75e3c
BlackDandel10n <blackdandel10n@proton.me>
2025-07-10 08:04:04 -0400

    Add JSON string validation

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ checks if a number string is valid JSON number
 is_valid_json_number("-123.456E+789")
 > True
 ```
+
+### is_valid_json_string(string)
+
+checks if a string is valid JSON string
+
+```python
+is_valid_json_number('"Hello there\\\n"')
+> True
+```
--- src/jyparse/parse.py
+++ src/jyparse/parse.py
@@ -5,3 +5,9 @@ def is_valid_json_number(s):
     if res is None:
         return False
     return res.string == s
+
+def is_valid_json_string(s):
+    res = re_comp(r"^\"([^\"\\]?(\\[\"\\\/bfnrt])?(\\u[0-9a-fA-F]{4})?)*\"$").match(s)
+    if res is None:
+        return False
+    return res.string == s
